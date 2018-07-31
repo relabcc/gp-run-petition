f5cec8e0db1252d8c3b0214dc1483188cfe220e8
Renddi Yeh <renddi@relab.cc>
2018-07-31 19:24:44 +0800

    try bulding

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: '最可怕的，不是跑不到終點 - 我要連署',
+    title: '最可怕的，不是跑不到終點',
     description: '【無拘無塑來跑步】當路跑活動的風潮日益成熟，你想像過最理想的路跑環境，應該具備什麼條件？',
   },
   plugins: [
